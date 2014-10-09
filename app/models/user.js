ca18957b89d7bff99fdd7c17598239de7b37cee3
Hack Reactor Students <accounts@hackreactor.com>
2014-10-09 11:06:12 -0700

    Users can log in

--- app/models/user.js
+++ app/models/user.js
@@ -4,32 +4,17 @@ var Promise = require('bluebird');
 
 var Schema = mongoose.Schema;
 
+var hash = function (pass) {
+  return bcrypt.hashSync(pass, bcrypt.genSaltSync(10));
+};
+
 var usersSchema = new Schema({
   id:  Number,
   username: String,
-  password: String,
+  password: {type: String, set: hash},
   timestamp: {type: Date, default: Date.now }
 });
 
 var User = mongoose.model('Users', usersSchema);
-// .extend({
-//   tableName: 'users',
-//   hasTimestamps: true,
-//   initialize: function(){
-//     this.on('creating', this.hashPassword);
-//   },
-  // comparePassword: function(attemptedPassword, callback) {
-  //   bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-  //     callback(isMatch);
-  //   });
-  // },
-//   hashPassword: function(){
-//     var cipher = Promise.promisify(bcrypt.hash);
-//     return cipher(this.get('password'), null, null).bind(this)
-//       .then(function(hash) {
-//         this.set('password', hash);
-//       });
-//   }
-// });
 
 module.exports = User;
