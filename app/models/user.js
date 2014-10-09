6e26c3f3fd91218c2fb6b3431c33448e6ee86d10
Hack Reactor Students <accounts@hackreactor.com>
2014-10-08 20:58:15 -0700

    started to refactor loginUser

--- app/models/user.js
+++ app/models/user.js
@@ -18,11 +18,11 @@ var User = mongoose.model('Users', usersSchema);
 //   initialize: function(){
 //     this.on('creating', this.hashPassword);
 //   },
-//   comparePassword: function(attemptedPassword, callback) {
-//     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-//       callback(isMatch);
-//     });
-//   },
+  // comparePassword: function(attemptedPassword, callback) {
+  //   bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+  //     callback(isMatch);
+  //   });
+  // },
 //   hashPassword: function(){
 //     var cipher = Promise.promisify(bcrypt.hash);
 //     return cipher(this.get('password'), null, null).bind(this)
