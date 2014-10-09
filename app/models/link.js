b68fe89daf95cb1741b692f1ac586c54717a434f
Hack Reactor Students <accounts@hackreactor.com>
2014-10-08 20:44:15 -0700

    User can sign up

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,22 @@
-var db = require('../config');
+var mongoose = require('mongoose');
 var crypto = require('crypto');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function(){
-    this.on('creating', function(model, attrs, options){
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+var Schema = mongoose.Schema;
+
+var urlsSchema = new Schema({
+  id: Number,
+  url: String,
+  base_url: String,
+  code: String,
+  title: String,
+  visits: Number,
+  timestamp: {type: Date, default: Date.now }
 });
+
+var Link = mongoose.model('Link', urlsSchema);
+  // tableName: 'urls',
+  // hasTimestamps: true,
+  // defaults: {
+  //   visits: 0
 
 module.exports = Link;
--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,35 @@
-var db = require('../config');
+var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function(){
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
-    });
-  },
-  hashPassword: function(){
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
+var Schema = mongoose.Schema;
+
+var usersSchema = new Schema({
+  id:  Number,
+  username: String,
+  password: String,
+  timestamp: {type: Date, default: Date.now }
 });
+
+var User = mongoose.model('Users', usersSchema);
+// .extend({
+//   tableName: 'users',
+//   hasTimestamps: true,
+//   initialize: function(){
+//     this.on('creating', this.hashPassword);
+//   },
+//   comparePassword: function(attemptedPassword, callback) {
+//     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+//       callback(isMatch);
+//     });
+//   },
+//   hashPassword: function(){
+//     var cipher = Promise.promisify(bcrypt.hash);
+//     return cipher(this.get('password'), null, null).bind(this)
+//       .then(function(hash) {
+//         this.set('password', hash);
+//       });
+//   }
+// });
 
 module.exports = User;
